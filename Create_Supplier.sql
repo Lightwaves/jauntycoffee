08b33b990cb4246faaf74cd759cfa62f988e3bb0
Stephen Ramos <storamos@gmail.com>
2021-11-14 17:13:30 -0500

    SQL to create Supplier's table

---
+++ Create_Supplier.sql
@@ -0,0 +1,13 @@
+Create TABLE Supplier(
+    supplier_id serial primary key,
+    company_name varchar(50),
+    country varchar(30),
+    sales_contract_name varchar(60),
+    email varchar(50) NOT NULL
+
+
+
+
+
+
+)
